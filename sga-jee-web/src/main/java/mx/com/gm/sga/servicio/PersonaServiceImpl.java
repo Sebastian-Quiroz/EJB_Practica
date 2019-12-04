f7a32cda06b114bca2de98df3ee4e8fa5fc792da
Sebastian Quiroz <sebastian.122@hotmail.com>
2019-12-04 10:51:44 -0500

    Se crea el proyecto JavaWeb "sga-jee-web" y Se agrega la Imagen ArquitecturaProyecto y se traen las clases Persona, PersonaServiceImpl y la interface PersonaServiceRemote, estas tres del proyecto anterior sga.jee

---
+++ sga-jee-web/src/main/java/mx/com/gm/sga/servicio/PersonaServiceImpl.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.com.gm.sga.servicio;
+
+import java.util.ArrayList;
+import javax.ejb.Stateless;
+import mx.com.gm.sga.domain.Persona;
+
+/**
+ *
+ * @author ADMIN
+ */
+
+@Stateless
+public class PersonaServiceImpl implements PersonaServiceRemote{
+
+    @Override
+    public ArrayList<Persona> listarPersonas() {
+        ArrayList<Persona> personas = new ArrayList<>();
+        personas.add(new Persona(1, "Sebastian", "Quiroz", "se@hotmail.com", "555-55-55"));
+        personas.add(new Persona(2, "Adriana", "Mora", "adri@gmail.com", "777-777-77"));
+        return personas;
+    }
+
+    @Override
+    public Persona encontrarPersonaPorId(Persona persona) {
+        return null;
+    }
+
+    @Override
+    public Persona encontrarPersonaPorEmail(Persona persona) {
+        return null;
+    }
+
+    @Override
+    public void registrarPersona(Persona persona) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void modificarPersona(Persona persona) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void eliminarPersona(Persona persona) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
