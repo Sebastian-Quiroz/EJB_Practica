eef4ce1876eff4cf9fecfc4b6b4d5f59e82030f3
Sebastian Quiroz <sebastian.122@hotmail.com>
2019-12-05 10:46:44 -0500

    Se crea los archivos de configuracion xml de log4j2 y persistence dentro del folder Resources y se crea una clase de prueba para verificar la persistencia de los datos llamada ClienteEntidadPersona

--- PruebasConexion/HolaMundo-JPA/src/main/java/mx/com/gm/sga/domain/Persona.java
+++ PruebasConexion/HolaMundo-JPA/src/main/java/mx/com/gm/sga/domain/Persona.java
@@ -9,6 +9,7 @@ import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
@@ -16,7 +17,7 @@ public class Persona implements Serializable{
     private static final long serialVersionUID = 1L;
     
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="id_persona")
     private int idPersona;
     
